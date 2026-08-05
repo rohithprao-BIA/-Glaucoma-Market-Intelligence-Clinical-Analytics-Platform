7b6c08c2a2593216f16bce9c35ba8b5f202e4c42
rohithprao-BIA <rohithprao@gmail.com>
2026-08-05 23:26:00 +0530

    Delete Q07_Preservative_Free_Portfolio_By_Company.sql

--- Q07_Preservative_Free_Portfolio_By_Company.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-       C.Company_Name,
-COUNT(P.preservative_Category) AS preservative_Category
-FROM Products AS P
-INNER JOIN Companies AS C
-ON P.company_id= C.company_id
-WHere preservative_Category= 'Preservative free'
-GROUP BY C.Company_Name
-ORDER BY C.Company_Name Desc;
